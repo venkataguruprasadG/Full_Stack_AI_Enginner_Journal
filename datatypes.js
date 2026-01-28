d21c1696dd56798ba49ea4611fb17eefb75c7ef8
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-28 11:54:59 +0530

    datatypes problem

---
+++ datatypes.js
@@ -0,0 +1,35 @@
+/* Problem Statement
+In your VS Code practice.js, create these 5 variables with proper data types:
+
+city = "Quthbullapur" (string)
+
+pincode = 500043 (number)
+
+isStudent = false (boolean)
+
+graduationYear = null (null)
+
+nextTopic (leave undefined)
+
+Print each variable, then use typeof on each to see their types. Expected: 5 values + 5 types*/
+
+let city = "india"
+let pincode = 500043
+let isStudent = false
+let graduationYear = null
+let nextTopic;
+
+console.log(city);
+console.log(typeof city);
+
+console.log(pincode);
+console.log(typeof pincode);
+
+console.log(isStudent);
+console.log(typeof isStudent);
+
+console.log(graduationYear);
+console.log(typeof graduationYear);
+
+console.log(nextTopic);
+console.log(typeof nextTopic);
