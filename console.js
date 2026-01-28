dc9ae94fae07cfcda6ff63ecadd78a9739a126c1
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-28 11:43:38 +0530

    console problem

---
+++ console.js
@@ -0,0 +1,6 @@
+/* Problem Statement
+Write console.log statements in the browser console to output: your name, your age as a number (e.g., 25), and the phrase "I am learning JavaScript".
+Expected output: three separate lines like "John", 25, "I am learning JavaScript".*/
+console.log("Guru Prasad");
+console.log(30);
+console.log("I'm learning JavaScript");
