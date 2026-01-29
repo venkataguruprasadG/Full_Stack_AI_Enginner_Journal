73b739c8cf3b43ac1faaa8f00d198fa11f23043e
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 15:20:05 +0530

    function parameter problem 15

---
+++ functionpara.js
@@ -0,0 +1,16 @@
+/* Problem Statement
+Create function welcome(name) that:
+
+Takes name parameter
+
+Prints "Welcome to JS, [name]!"
+
+Call it with your name
+
+Expected: Welcome to JS, Guru Prasad! */
+
+function welcome(name) {
+    console.log(`Welcome to JS, ${name}!`)
+}
+
+welcome("Guru Prasad");
