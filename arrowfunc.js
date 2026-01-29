2bffa872eeda67ed37aea739245e28494b93f9cc
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 19:14:14 +0530

    arrow function problem 25.

---
+++ arrowfunc.js
@@ -0,0 +1,15 @@
+/* Problem Statement
+Array: scores = [85, 92, 78]
+
+Use forEach() with arrow function (score) => {} to:
+
+Print each score prefixed with "Score: "
+
+Expected: 3 lines total
+
+*/
+let scores = [85, 92, 78];
+
+scores.forEach((score) => {
+    console.log("Score:", score);
+});
