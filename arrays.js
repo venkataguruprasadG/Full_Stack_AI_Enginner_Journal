051f00ac6e56b5e84c64c7fb0cca86b37b4f6172
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 15:07:02 +0530

    array problem 12.

---
+++ arrays.js
@@ -0,0 +1,16 @@
+/* Problem Statement
+Create array: skills = ["HTML", "CSS", "JavaScript"]
+
+Print skills[1] (middle item)
+
+Print skills.length
+
+Print all 3 items using separate console.log(skills[0]) */
+
+let skills = ["HTML", "CSS", "JAVASCRIPT"];
+
+console.log(skills[0]);
+console.log(skills[1]);
+console.log(skills[2]);
+
+console.log(skills.length);
