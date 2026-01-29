06668944b2a5220108be1d5b4bec0375b594e79a
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 17:08:36 +0530

    object problem 17.

---
+++ object.js
@@ -0,0 +1,29 @@
+/* Problem Statement
+Create object student with:
+
+name: "Guru Prasad"
+
+age: 30
+
+city: "Quthbullapur"
+
+skills: ["JS", "HTML"]
+
+Print:
+
+student.name
+
+student.city
+
+student.skills[0] */
+
+let student = {
+    name: "guru prasad",
+    age: 30,
+    city: "Qutubullapur",
+    skills: ["JS", "HTML"]
+}
+
+console.log(student.name);
+console.log(student.city);
+console.log(student.skills[0]);
