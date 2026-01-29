18045217ae0ee0f48980b8bfd46e58d9c540e414
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 15:12:16 +0530

    array methods problem 13.

---
+++ arraymethod.js
@@ -0,0 +1,18 @@
+/* Problem Statement
+Start with: colors = ["red", "green"]
+
+Add "blue" using push()
+
+Remove last color using pop()
+
+Print final array length
+
+Expected output: 2 */
+
+let colors = ["red", "green"];
+
+colors.push("Blue");
+
+colors.pop();
+
+console.log(colors.length);
