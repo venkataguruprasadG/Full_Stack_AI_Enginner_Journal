a14342117460e8ed2c85756322830f9ba59855d6
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 18:54:37 +0530

    foreach problem 24.

---
+++ foreach.js
@@ -0,0 +1,16 @@
+/* Problem Statement
+Array: languages = ["JavaScript", "Python", "Java"]
+
+Use forEach() to:
+
+Print each language prefixed with "Learn: "
+
+Expected: 3 lines total
+
+Expected output*/
+
+let languages = ["javaScript", "Python", "java"];
+
+languages.forEach(function (language) {
+    console.log("Learn:", language);
+});
