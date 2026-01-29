017f28fe90e065cb06967fcf09088bfbfc997b75
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 17:28:30 +0530

    math problem 20.

---
+++ math.js
@@ -0,0 +1,15 @@
+/* Problem Statement
+Print these results:
+
+Random number (0-1)
+
+Math.floor(7.9)
+
+Math.ceil(5.1) (rounds UP)
+
+Math.max(100, 500, 200)*/
+
+console.log(Math.random(0, 1));
+console.log(Math.floor(7.9));
+console.log(Math.ceil(5.1));
+console.log(Math.max(100, 500, 200));
