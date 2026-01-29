0af5d2c67f48d78e76afecdfcef272cb5905f25e
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 18:48:26 +0530

    array iteration problem.

---
+++ arrayiteration.js
@@ -0,0 +1,14 @@
+/* Problem Statement
+Array: cities = ["Hyderabad", "Bangalore", "Chennai"]
+
+Use for...of loop to:
+
+Print each city name prefixed with "City: "
+
+Expected: 3 lines total*/
+
+let citites = ["Hyderabad", "Bangalore", "Chennai"]
+
+for (let city of citites) {
+    console.log("City:", city);
+}
