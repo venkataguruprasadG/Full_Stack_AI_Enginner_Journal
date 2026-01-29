fa2b3184510e4d8db2c03be5e0f8a9f918e6e807
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 17:25:50 +0530

    string methods

---
+++ stringmethods.js
@@ -0,0 +1,13 @@
+/* Problem Statement
+Variable: city = "Qutubullapur"
+
+Print city in UPPERCASE
+
+Print city length
+
+Check if city includes "bull" → true/false */
+
+let city = "Qutubullapur";
+console.log(city.toUpperCase());
+console.log(city.length);
+console.log(city.includes("bull"));
