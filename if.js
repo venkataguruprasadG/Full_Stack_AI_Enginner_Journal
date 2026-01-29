21e514962f4ca1f59aba906fa1a807fa8c5460d0
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 13:44:53 +0530

    if condition.

---
+++ if.js
@@ -0,0 +1,21 @@
+/* Problem Statement
+Variables: balance = 500, price = 700
+
+Check if (balance >= price) → Print "Can buy!" or "Insufficient funds"
+
+let temp = 35 → If temp > 30 print "Hot weather!"*/
+
+let balance = 500;
+let price = 700;
+
+if (balance >= price) {
+    console.log("Can Buy!");
+} else {
+    console.log("Insufficient Funds");
+}
+
+let temp = 35;
+
+if (temp > 30) {
+    console.log("Hot Weather!");
+}
