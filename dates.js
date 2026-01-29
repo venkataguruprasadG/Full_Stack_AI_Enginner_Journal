c20a48e20f0381a9fb90e714df0c8f90b4d60476
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 17:36:14 +0530

    dates problem 21

---
+++ dates.js
@@ -0,0 +1,16 @@
+/* Create today = new Date() and print:
+
+Full year (getFullYear())
+
+Month (0-11, getMonth())
+
+Day of week (0-6, getDay() → Thursday = 4)
+
+Day of month (getDate())*/
+
+let today = new Date();
+
+console.log(today.getFullYear());
+console.log(today.getMonth());
+console.log(today.getDay());
+console.log(today.getDate());
