dd25f89ac2935adaf47362b1ee0c9cc550f4e7c7
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 14:59:34 +0530

    for problem 11

---
+++ for.js
@@ -0,0 +1,14 @@
+/* Problem Statement
+Create for loop that:
+
+Starts at i = 0
+
+Runs while i < 6
+
+Prints even numbers only: 0, 2, 4 (use i += 2)
+
+Expected output: 0, 2, 4 on separate lines */
+
+for (let i = 0; i < 6; i += 2) {
+    console.log(i);
+}
