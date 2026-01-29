8c4183256b70ee00d54af1f6776efcc7a410a908
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 15:14:50 +0530

    functions problem 14.

---
+++ functions.js
@@ -0,0 +1,17 @@
+/* Problem Statement
+Create function printSkills() that:
+
+Contains array skills = ["HTML", "CSS", "JS"]
+
+Prints all 3 skills using console.log
+
+Call the function once */
+
+function printSkills() {
+    let skills = ["HTML", "CSS", "JS"];
+    console.log(skills[0]);
+    console.log(skills[1]);
+    console.log(skills[2]);
+}
+
+printSkills();
