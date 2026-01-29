bf0d795edc14330de13cdfc726fc52a721c7acb5
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 15:22:42 +0530

    return problem 16.

---
+++ return.js
@@ -0,0 +1,19 @@
+/* Problem Statement
+Create function multiply(num1, num2) that:
+
+Takes 2 parameters
+
+Returns their product (num1 * num2)
+
+Call it: let result = multiply(4, 6)
+
+Print result
+
+Expected: 24 */
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+let result = multiply(4, 6);
+console.log(result);
