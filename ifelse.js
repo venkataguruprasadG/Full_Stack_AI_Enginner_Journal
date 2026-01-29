7c241aeb86f64e86211bc9032f27c363156ee5c3
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 13:56:24 +0530

    if else problem7

---
+++ ifelse.js
@@ -0,0 +1,23 @@
+/* Problem Statement
+Variables: score = 85, temp = 25
+
+if (score >= 90) print "Excellent!" else "Good job!"
+
+if (temp > 30) print "Hot!" else "Pleasant!"
+
+Use two separate if-else statements */
+
+let score = 85;
+let temp = 25;
+
+if (score >= 90) {
+    console.log("Excellent!");
+} else {
+    console.log("Good Job");
+}
+
+if (temp > 30) {
+    console.log("Hot");
+} else {
+    console.log("Pleasant");
+}
