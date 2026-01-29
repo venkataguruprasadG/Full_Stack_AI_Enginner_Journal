2dbde5556445860dce9db8db1ca1f43b0c14a1da
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 12:28:48 +0530

    comparison operators problem 4

---
+++ comparisonOperatos.js
@@ -0,0 +1,20 @@
+/*Create variables: score = 85, passMark = 70, playerLevel = 5
+
+Test and print:
+
+Is score >= passMark?
+
+Is playerLevel != 3?
+
+Is score > 90?
+
+Expected: true, true, false*/
+
+let score = 85
+let passMark = 70
+let playerLevel = 5
+
+console.log(score >= passMark);
+console.log(playerLevel != 3);
+console.log(score > 90);
+
