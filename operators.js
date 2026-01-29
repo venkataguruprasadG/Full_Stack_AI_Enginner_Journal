7aa843774b091d9127ed7e04a2df4ad2496149ee
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 12:24:48 +0530

    operators problem.

---
+++ operators.js
@@ -0,0 +1,22 @@
+/* Problem Statement
+Using your arithmetic operators knowledge:
+
+Create variables: myAge = 30, siblingAge = 25, pocketMoney = 1000
+
+Calculate: age difference, double the money, remainder after dividing by 300
+
+Print all 3 results
+
+Expected: 5, 2000, 100 on separate lines. */
+
+let myAge = 30;
+let siblingAge = 25;
+let pocketMoney = 1000;
+
+ageDifference = myAge - siblingAge;
+doubleMoney = pocketMoney * 2;
+remainingMoney = pocketMoney % 300;
+
+console.log(ageDifference);
+console.log(doubleMoney);
+console.log(remainingMoney);
