13ea4a56e3ef4879c47c353f5ae1efb3bd3b6ec1
venkataguruprasadG <venkatagp27@gmail.com>
2026-01-29 14:56:24 +0530

    while problem 10

---
+++ while.js
@@ -0,0 +1,17 @@
+/* Problem Statement
+Variable: i = 1
+
+While loop that:
+
+Prints numbers 1 to 10
+
+Uses i++ to increment
+
+Expected output: 10 lines (1 through 10 */
+
+let i = 1;
+
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
