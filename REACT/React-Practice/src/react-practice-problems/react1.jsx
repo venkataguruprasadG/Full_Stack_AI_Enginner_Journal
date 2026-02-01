77be1aa75de23299fdb183564e94ab771af6f711
venkataguruprasadG <venkatagp27@gmail.com>
2026-02-01 10:58:48 +0530

    react 1 problem is done with proper setup.

---
+++ REACT/React-Practice/src/react-practice-problems/react1.jsx
@@ -0,0 +1,11 @@
+function React1() {
+    const myName = "Guru Prasad";
+    return (
+        <div>
+            <h1>Hello, {myName}!</h1>
+            <p>Welcome to React practice problems!</p>
+        </div>
+    )
+}
+
+export default React1;
