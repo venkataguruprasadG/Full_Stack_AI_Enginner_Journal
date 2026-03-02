131b5007fc922024c7f8a6770fb67a193e62af16
venkataguruprasadG <venkatagp27@gmail.com>
2026-03-02 23:44:46 +0530

    project 2 user profile card jsx code written.

---
+++ REACT/React-Practice/src/react-practice-problems/project2/project2.jsx
@@ -0,0 +1,19 @@
+import './project2.css'
+
+function Project2() {
+    // Variables inside the component
+    const userName = "John Doe";
+    const jobTitle = "Software Engineer";
+    const location = "Hyderabad";
+
+    return (
+        <div className="container">
+            <h1 className='title'>Profile Snippet</h1>
+            <p className='description-name'>Name: {userName}</p>
+            <p className='description-job'>Job Title: {jobTitle}</p>
+            <p className='description-location'>Location: {location}</p>
+        </div>
+    )
+}
+
+export default Project2;
