22658e0d69c0accb09da8b19cbf905abf6d21908
venkataguruprasadG <venkatagp27@gmail.com>
2026-03-09 01:54:11 +0530

    adivse giver project done.

--- REACT/React-Practice/src/react-practice-problems/project16.jsx
+++ REACT/React-Practice/src/react-practice-problems/project16.jsx
@@ -1,2 +1,26 @@
 import { useState } from "react";
 import { useEffect } from "react";
+
+function Project16() {
+    const [advice, setAdvice] = useState("Loading Advice...");
+
+    const fetchAdvice = async () => {
+        const response = await fetch("https://api.adviceslip.com/advice");
+        const data = await response.json();
+        setAdvice(data.slip.advice);
+    };
+
+    useEffect(() => {
+        fetchAdvice();
+    }, []);
+
+    return (
+        <div>
+            <h1>Daily Wisdom</h1>
+            <p>{advice}</p>
+            <button onClick={fetchAdvice}>Give me more advice!</button>
+        </div>
+    );
+}
+
+export default Project16;
